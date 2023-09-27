98b69cd040d7ac0ffe3dbf677c000ec9a31b6a07
Muhammad Tayyab Mir <tayyabmir056@gmail.com>
2023-09-27 07:08:01 +0500

    correlate chemical points with created rasters

--- main.py
+++ main.py
@@ -87,11 +87,43 @@ def create_rasters(output_path):
 
 
         raster_filename = output_path + "/" + col + ".tif"
-        text_box.insert("end", "Creating Raster: " + raster_filename + "\n")
+        text_box.insert("end", "using "+ "invdist:power={}smoothing={}:radius1={}:radius2={}:angle={}:max_points={}:min_points={}:nodata={}".format(power_entry.get(), smoothing_entry.get(), radius_entry.get(), radius_entry.get(), angle_entry.get(), max_points_entry.get(), min_points_entry.get(), nodata_entry.get()) + "\n")
         if os.path.exists(raster_filename):
             driver.DeleteDataSource(raster_filename)
-        gdal.Grid(raster_filename, src_shp, zfield='value', format="GTiff", outputType=output_type, outputSRS="EPSG:4326", noData=nodata, algorithm= "invdist:power=3.0:smoothing=0.0006:radius1=1500:radius2=1500:angle=0.0:max_points=0:min_points=0:nodata=0.0")
+        text_box.insert("end", "using: " + raster_filename + "\n")
+        gdal.Grid(raster_filename, src_shp, zfield='value', format="GTiff", outputType=output_type, outputSRS="EPSG:4326", noData=nodata, algorithm= "invdist:power={}smoothing={}:radius1={}:radius2={}:angle={}:max_points={}:min_points={}:nodata={}".format(power_entry.get(), smoothing_entry.get(), radius_entry.get(), radius_entry.get(), angle_entry.get(), max_points_entry.get(), min_points_entry.get(), nodata_entry.get()))
 
+    # for all rasters, get the pixel value for each point in the chemical csv file and save it in a new csv file
+    dataset = pd.read_csv(chemical_csv_file)
+    for col in columns_to_check:
+        raster_filename = output_path + "/" + col + ".tif"
+        dataset[col] = dataset.apply(lambda row: get_pixel_value(row["Longitude"], row["Latitude"], get_geo_transform(raster_filename), get_raster_data(raster_filename)), axis=1)
+    
+    csv_path = output_path.replace("rasters", "results")
+    if not os.path.exists(csv_path):
+        os.makedirs(csv_path)
+    
+    dataset.to_csv(csv_path + "/results.csv", index=False)
+
+def get_raster_data(raster_filename):
+    raster = gdal.Open(raster_filename)
+    band = raster.GetRasterBand(1)
+    return band.ReadAsArray()
+
+def get_geo_transform(raster_filename):
+    raster = gdal.Open(raster_filename)
+    return raster.GetGeoTransform()
+
+def get_pixel_value(x, y, geotransform, raster_data):
+    origin_x = geotransform[0]
+    origin_y = geotransform[3]
+    pixel_width = geotransform[1]
+    pixel_height = -geotransform[5]
+
+    pixel_x = int((x - origin_x) / pixel_width)
+    pixel_y = int((y - origin_y) / pixel_height)
+
+    return raster_data[pixel_y, pixel_x]
 
 def save_file():
     #Open file dialog to ask for folder
@@ -104,28 +136,86 @@ def save_file():
         
         # Create shapefiles
         create_shapefiles(shapefile_path)
+
+        text_box.insert("end", "Process Finished \n")
         
 
 # Create the main application window
 root = tk.Tk()
-root.geometry("350x450")
+root.geometry("350x900")
 root.title("CSV File Reader")
 
-# Create a label
+
 label = tk.Label(root, text="CSV File Reader", font=("Arial", 16))
 label.pack(pady=10)
 
- 
-upload_button = tk.Button(root, text="Upload Soil CSV", command=open_sensor_file)
-upload_button.pack(pady=5)
+upload_button = tk.Button(root, text="Upload Sensors data CSV", command=open_sensor_file)
+upload_button.pack(pady=5, anchor=tk.W, padx=5)
  
 upload_button2 = tk.Button(root, text="Upload Chemical CSV", command=open_chemical_file)
-upload_button2.pack(pady=5)
+upload_button2.pack(pady=5, anchor=tk.W, padx=5)
+
+# Parameters
+label2 = tk.Label(root, text="Parameters", font=("Arial", 14))
+label2.pack(pady=10)
+
+#power = 3
+label_power = tk.Label(root, text="Power", font=("Arial", 12))
+label_power.pack(pady=5, anchor=tk.W, padx=5)
+power_entry = tk.Entry(root)
+power_entry.pack(pady=5, anchor=tk.W, padx=5)
+power_entry.insert(0, "3")
+power_entry.pack(anchor=tk.W, padx=5, pady=5)
+
+#smoothing = 0.0006
+label_smoothing = tk.Label(root, text="Smoothing", font=("Arial", 12))
+label_smoothing.pack(anchor=tk.W, padx=5, pady=5)
+smoothing_entry = tk.Entry(root)
+smoothing_entry.pack(anchor=tk.W, padx=5, pady=5)
+smoothing_entry.insert(0, "0.0006")
+
+#radius= 1500
+label_radius = tk.Label(root, text="Radius", font=("Arial", 12))
+label_radius.pack(pady=5, padx=5, anchor=tk.W)
+radius_entry = tk.Entry(root)
+radius_entry.pack(pady=5, padx=5, anchor=tk.W)
+radius_entry.insert(0, "1500")
+
+#angle: 0
+label_angle = tk.Label(root, text="Angle", font=("Arial", 12))
+label_angle.pack(pady=5, padx=5, anchor=tk.W)
+angle_entry = tk.Entry(root)
+angle_entry.pack(pady=5, padx=5, anchor=tk.W)
+angle_entry.insert(0, "0")
+
+#max_points = 0
+label_max = tk.Label(root, text="Max Points", font=("Arial", 12))
+label_max.pack(pady=5, padx=5, anchor=tk.W)
+max_points_entry = tk.Entry(root)
+max_points_entry.pack(pady=5, padx=5, anchor=tk.W)
+max_points_entry.insert(0, "0")
+
+#min_points = 0
+label_min = tk.Label(root, text="Min Points", font=("Arial", 12))
+label_min.pack(pady=5, padx=5, anchor=tk.W)
+min_points_entry = tk.Entry(root)
+min_points_entry.pack(pady=5, padx=5, anchor=tk.W)
+min_points_entry.insert(0, "0")
+
+#nodata = 0
+label_nodata = tk.Label(root, text="No Data", font=("Arial", 12))
+label_nodata.pack(pady=5, padx=5, anchor=tk.W)
+nodata_entry = tk.Entry(root)
+nodata_entry.pack(pady=5, padx=5, anchor=tk.W)
+nodata_entry.insert(0, "0")
+
  
 upload_button3 = tk.Button(root, text="Save CSV", command=save_file)
 upload_button3.pack(pady=5)
 
 ## Text box for logs
+lable_logs = tk.Label(root, text="Logs", font=("Arial", 14))
+lable_logs.pack(pady=5, anchor=tk.W, padx=5)
 text_box = tk.Text(root, height=10, width=30)
 text_box.pack(pady=10)
 
