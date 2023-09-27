2c135d3d2df6ca93c0d9c8178248b0cde2b234a9
Muhammad Tayyab Mir <tayyabmir056@gmail.com>
2023-09-27 05:42:07 +0500

    create shapefiles and rasters from csv complete

--- main.py
+++ main.py
@@ -2,67 +2,109 @@ import tkinter as tk
 from tkinter import filedialog,messagebox  
 import csv
 from tkinter import ttk
+from osgeo import ogr, osr, gdal, gdalconst
+import numpy as np
+import pandas as pd
+import os
 
-def open_file():
-    file_path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
-    if file_path:
-        # Clear the previous table content, if any
-        clear_table()
-        
-        # Read and display the CSV content in the table
-        read_csv(file_path)
 
-def clear_table():
-    for row in table.get_children():
-        table.delete(row)
+sensors_csv_file = ""
+chemical_csv_file = ""
+columns_to_check= ["232-Th", "238-U", "40-K", "137-Cs", "Countrate"]
 
-def read_csv(file_path):
-    try:
-        with open(file_path, newline="") as csvfile:
-            reader = csv.reader(csvfile)
-            header = next(reader)
-            
-            # If the table doesn't have columns yet, set them based on the CSV header
-            if not table["columns"]:
-                table["columns"] = header
-                for col in header:
-                    table.heading(col, text=col)
-                    table.column(col, width=100)
-            
-            # Populate the table with CSV data
-            for row_data in reader:
-                table.insert("", "end", values=row_data)
-    except FileNotFoundError:
-        print("File not found.")
-    except csv.Error as e:
-        error_message = f"CSV Error: {e}"
-        print(error_message)
-        messagebox.showerror("CSV Error", error_message)
-        clear_table()  # Clear the table if there's an error
+
+def open_sensor_file():
+    global sensors_csv_file 
+    sensors_csv_file= filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
+    if sensors_csv_file:
+        text_box.insert("end", "reading file: " + sensors_csv_file+ "\n")
+
+def open_chemical_file():
+    global chemical_csv_file
+    chemical_csv_file = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
+    if chemical_csv_file:
+        text_box.insert("end", "reading file: " + chemical_csv_file+ "\n")
+
+
+def create_shapefiles(output_path):
+    driver = ogr.GetDriverByName("ESRI Shapefile")
+    
+    # Spatial reference
+    srs = osr.SpatialReference()
+    srs.ImportFromEPSG(4326)  # WGS84
+
+    dataset = pd.read_csv(sensors_csv_file)
+    for col in columns_to_check:
+        shp_filename = output_path + "/" + col + ".shp"
+        text_box.insert("end", "Creating Shapefile: " + shp_filename + "\n")
+        if os.path.exists(shp_filename):
+            driver.DeleteDataSource(shp_filename)
+        ds = driver.CreateDataSource(shp_filename)
+        layer = ds.CreateLayer(col, srs, ogr.wkbPoint)
+        layer.CreateField(ogr.FieldDefn("id", ogr.OFTInteger))
+        layer.CreateField(ogr.FieldDefn("value", ogr.OFTReal))
+        layer.CreateField(ogr.FieldDefn("lat", ogr.OFTReal))
+        layer.CreateField(ogr.FieldDefn("lon", ogr.OFTReal))
+
+        for index, row in dataset.iterrows():
+            feature = ogr.Feature(layer.GetLayerDefn())
+            feature.SetField("id", index)
+            feature.SetField("value", row[col])
+            feature.SetField("lat", row["lat"])
+            feature.SetField("lon", row["lon"])
+            wkt = "POINT(%f %f)" % (float(row["lon"]), float(row["lat"]))
+            point = ogr.CreateGeometryFromWkt(wkt)
+            feature.SetGeometry(point)
+            layer.CreateFeature(feature)
+            feature.Destroy()
+        ds.Destroy()
+
+    # Create rasters
+    rasters_path = output_path.replace("shapefiles", "rasters")
+    if not os.path.exists(rasters_path):
+        os.makedirs(rasters_path)
+    create_rasters(rasters_path)
+
+def create_rasters(output_path):
+
+    # Create rasters using inverse distance weighting grid
+    power = 3
+    smoothing = 0.0006
+    radius= 1500
+    angle: 0
+    max_points = 0
+    min_points = 0
+    nodata = 0
+    output_type = gdalconst.GDT_Float32
+    for col in columns_to_check:
+        src_shp = output_path.replace("rasters", "shapefiles") + "/" + col + ".shp"
+
+        # get shp bounds
+        ds = ogr.Open(src_shp)
+        layer = ds.GetLayer()
+        extent = layer.GetExtent()
+        ds.Destroy()
+
+
+        raster_filename = output_path + "/" + col + ".tif"
+        text_box.insert("end", "Creating Raster: " + raster_filename + "\n")
+        if os.path.exists(raster_filename):
+            driver.DeleteDataSource(raster_filename)
+        gdal.Grid(raster_filename, src_shp, zfield='value', format="GTiff", outputType=output_type, outputSRS="EPSG:4326", noData=nodata, algorithm= "invdist:power=3.0:smoothing=0.0006:radius1=1500:radius2=1500:angle=0.0:max_points=0:min_points=0:nodata=0.0")
 
 
 def save_file():
-    if table.get_children():
- 
-        file_path = filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("CSV Files", "*.csv")])
-        if file_path:
-            try:
-                with open(file_path, mode="w", newline="") as csvfile:
-                    writer = csv.writer(csvfile)
-                    
-                    # Write CSV header
-                    header = [table.heading(col)["text"] for col in table["columns"]]
-                    writer.writerow(header)
-                    
-                    # Write CSV data
-                    for item in table.get_children():
-                        row_data = [table.item(item, "values")[i] for i in range(len(header))]
-                        writer.writerow(row_data)
-            except csv.Error as e:
-                print(f"CSV Error: {e}")
-    else:
-               messagebox.showinfo("No Data", "No data to save. Please upload a CSV file.")  # Show a message box
+    #Open file dialog to ask for folder
+    folder_path = filedialog.askdirectory()
+    if folder_path:
+        shapefile_path = folder_path + "/shapefiles"
 
+        if not os.path.exists(shapefile_path):
+            os.makedirs(shapefile_path)
+        
+        # Create shapefiles
+        create_shapefiles(shapefile_path)
+        
 
 # Create the main application window
 root = tk.Tk()
@@ -74,29 +116,18 @@ label = tk.Label(root, text="CSV File Reader", font=("Arial", 16))
 label.pack(pady=10)
 
  
-upload_button = tk.Button(root, text="Upload Soil CSV", command=open_file)
+upload_button = tk.Button(root, text="Upload Soil CSV", command=open_sensor_file)
 upload_button.pack(pady=5)
  
-upload_button2 = tk.Button(root, text="Upload Chemical CSV", command=open_file)
+upload_button2 = tk.Button(root, text="Upload Chemical CSV", command=open_chemical_file)
 upload_button2.pack(pady=5)
- 
-clear_button = tk.Button(root, text="Clear", command=clear_table)
-clear_button.pack(pady=5)
-
-# Create a frame for the table to make it 800x800
-table_frame = tk.Frame(root, width=800, height=800)
-table_frame.pack(padx=3, pady=3)
-
-# Create a table to display CSV content within the frame
-table = ttk.Treeview(table_frame, show="headings")   
- 
-table["columns"] = ("Email" ) 
-table.heading("Email", text="Email")   
  
 upload_button3 = tk.Button(root, text="Save CSV", command=save_file)
 upload_button3.pack(pady=5)
 
-table.pack(fill="both", expand=True)
+## Text box for logs
+text_box = tk.Text(root, height=10, width=30)
+text_box.pack(pady=10)
 
 # Start the Tkinter main loop
 root.mainloop()
